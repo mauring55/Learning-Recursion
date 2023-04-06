aef13a3e4972578b1557334a943b40857569f04e
mauring55 <83412888+mauring55@users.noreply.github.com>
2023-04-06 14:49:20 +0800

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -26,5 +26,18 @@ def iter_factorial(p):
 
 print (iter_factorial(4))
 
+def printMo
 
 
+ef printMove(fr, to):
+  print('move from ' + str(fr) + ' to ' + str(to))
+ 
+def Towers(n, fr, to, spare):
+  if n == 1:
+    printMove(fr, to)
+  else:
+    Towers(n-1, fr, spare, to)
+    Towers(1, fr, to, spare)
+    Towers(n-1, spare, to, fr)
+
+Towers(4, 'fr', 'to', 'spare')
