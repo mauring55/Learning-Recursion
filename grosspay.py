b5f46abbd9f6fe3e1aedc3d61a5c60c5e29fbabd
mauring55 <83412888+mauring55@users.noreply.github.com>
2023-04-09 13:13:51 +0000

    modified grosspay.py and added gradeconverter.py

--- grosspay.py
+++ grosspay.py
@@ -1,6 +1,21 @@
-hrs = input('Enter hours:')
-rate = input('Enter pay rate:')
+#simple gross pay calculator
 
-gross_pay = float(hrs) * float(rate)
+#hrs = input('Enter hours:')
+#rate = input('Enter pay rate:')
 
-print('Pay:', gross_pay)
+#gross_pay = float(hrs) * float(rate)
+
+#print('Pay:', gross_pay)
+
+
+
+#modified gross pay calculator
+hrs = float(input('Enter hours:'))
+rate = float(input('Enter rate:'))
+
+if hrs <= 40:
+  gross_pay = hrs * rate
+else:
+  gross_pay = 40 * rate + ((hrs - 40) * (1.5 * rate))
+
+print(gross_pay)
